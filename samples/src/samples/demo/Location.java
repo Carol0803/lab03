7f9bdf26274a24fbae000cdca8b6c7d9cd255c58
Wong Kak Lok <carolwong.0803@gmail.com>
2022-03-12 17:25:36 +0800

    commit location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
